984a1b9c9672652d5c3cd788b749a84ae9bbbe04
EL MTOUGUI Hanane <Hanane.EL-MTOUGUI@akkodis.com>
2023-03-16 12:03:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    {/* path="*" fonctionne si jamais l'url ne correspond à rien de déclaré au dessus */}
+                    <Route path="*" element={<Home />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+};
 
-export default App;
+export default App;// rsc=react stateless componenet utilise functional componenet
---
+++ src/pages/About.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum, dolor sit amet consectetur
+                adipisicing elit. Porro est mollitia nobis
+                optio voluptas veritatis unde debitis tempore
+                explicabo repudiandae velit totam, cupiditate
+                quod voluptatum exercitationem obcaecati animi
+                voluptatibus rem nesciunt deserunt! Beatae fuga
+                esse quasi reprehenderit? Ad quam odio accusamus
+                nulla voluptas! In, fugit sapiente! Quidem in dicta,
+                voluptatem praesentium non vitae est magni neque.
+                Sed aspernatur consectetur, beatae, et minus in
+                sunt repudiandae accusantium maxime quod amet
+                consequatur fugit. Sed asperiores distinctio
+                blanditiis delectus? Perferendis recusandae quod
+                quis earum illum est quo corporis. Cupiditate illum
+                eum corrupti eos sit autem ullam explicabo.
+                Earum exercitationem ab at quos quaerat.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur,
+                adipisicing elit. Quos ducimus nostrum ut
+                voluptates nulla doloremque cum saepe ad quidem
+                modi nesciunt rerum atque blanditiis repudiandae
+                veniam aspernatur necessitatibus eius laudantium
+                quam, minima delectus suscipit. Mollitia quae minima,
+                consequuntur voluptas consequatur nobis culpa recusandae
+                cum quaerat facere deserunt aut molestias quo, illo
+                reiciendis reprehenderit quas eligendi fugiat ipsa eaque
+                iste quis aliquid veniam enim. Consectetur quasi ullam
+                at saepe magnam possimus error, doloremque aliquid dolores?
+                Praesentium nihil illo quaerat quisquam ab adipisci
+                necessitatibus minima impedit est aliquid nostrum corporis,
+                similique alias blanditiis. Eaque iure omnis iste molestiae nihil,
+                laborum delectus deserunt illum id obcaecati excepturi aperiam
+                dignissimos soluta nulla. Unde quia quis non qui illo in possimus
+                repellat saepe. Soluta, at odio voluptatum debitis ipsa error dicta
+                obcaecati laudantium quis autem deserunt commodi in dolores laboriosam
+                distinctio doloribus labore impedit id reprehenderit eligendi voluptatem
+                exercitationem? Porro, voluptate ex. Quo velit est debitis quis ratione
+                deserunt consequuntur, fugiat laboriosam saepe provident minus corporis accusamus
+                voluptas accusantium perferendis officia sit non. Consectetur sunt voluptates maiores,
+                est ipsa illum libero ratione modi praesentium obcaecati delectus aliquam quisquam voluptate,
+                ipsam quidem facilis soluta suscipit quia error natus? Odit, mollitia!
+                Repudiandae aliquam dolore inventore corporis iure?</p>
+        </div>
+    );
+};
+
+export default About;
